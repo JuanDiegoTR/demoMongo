530b5eb5c6f9054f168a37ff451233edaabfea97
JuanDiegoTR <jtovarod@emeal.nttdata.com>
2024-07-16 11:15:24 -0500

    servicio findByNumIdentAndTipoIdent

---
+++ src/main/java/com/prueba/restapi/service/AfiliadoService.java
@@ -0,0 +1,17 @@
+package com.prueba.restapi.service;
+
+import com.prueba.restapi.dto.AfiliadoDTO;
+
+public interface AfiliadoService {
+
+    /**
+     * Metodo que consulta por tipo y numero de identificacion
+     *
+     * @param tipoIdentificacion tipo de identificacion del usuario
+     * @param numeroIdentificacion numero de identificacion del usuario
+     * @return informacion del usuario
+     * @throws Exception error
+     */
+    AfiliadoDTO findByNumIdentAndTipoIdent(String tipoIdentificacion, String numeroIdentificacion) throws Exception;
+
+}
